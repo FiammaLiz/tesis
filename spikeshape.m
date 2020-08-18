5632f6e594fa8d34a83b78982101c1737a2eeb0b
Fiamma <fiamma.liz17@gmail.com>
2020-08-17 23:47:24 -0300

    corrijo superposicion de spike shape de tetrodos porque le puse mal cuando empezar el siguiente, ahora esta bien

--- spikeshape.m
+++ spikeshape.m
@@ -19,7 +19,7 @@ t_ss= (1:length(spikeshapes(:,1)))/sample_rate; %tiempo que duran los spikes par
     sss(1)=subplot(3,1,1);
 for ch=1:numch %para cada canal
 for m=1:length(spike_lcs_ss{k}) %ploteo cada spike apilandolos en un plot por canal 
-    plot (t_ss+w_post*(ch-1),spikeshapes_ch(ch).ch(:,m),'color',[0.4940 0.1840 0.5560 0.3]); %color violeta con cierta transparencia, cuando se apilan se oscurece las partes donde coinciden
+    plot (t_ss+(w_pre+w_post)*(ch-1),spikeshapes_ch(ch).ch(:,m),'color',[0.4940 0.1840 0.5560 0.3]); %color violeta con cierta transparencia, cuando se apilan se oscurece las partes donde coinciden
     hold on
    % pause %por si quiero ir viendo los spikes mientras apila
 end 
@@ -32,12 +32,12 @@ title 'Spike shapes de un tetrodo'
 for ch=1:numch %para cada canal hago un subplot
     sss(2)=subplot(3,1,2);
 for m=1:length(spike_lcs_ss{k}) %ploteo cada spike apilandolos en un plot por canal 
-    plot (t_ss+w_post*(ch-1),spikeshapes_ch(ch).ch(:,m)), %multicolor si uso desvío estandard 
+    plot (t_ss+(w_pre+w_post)*(ch-1),spikeshapes_ch(ch).ch(:,m)), %multicolor si uso desvío estandard 
     hold on
 end 
     plot (t_ss+w_post*(ch-1), mean(spikeshapes_ch(ch).ch,2),'color',[0 0 0 0],'LineWidth',2); %ploteo la media superpuesta a los spikes
     desv_std= std(spikeshapes'); %calculo el desvio estandard de la media
-    errorbar(t_ss+w_post*(ch-1),mean(spikeshapes_ch(ch).ch,2),desv_std,'color',[0 0 0 0],'LineWidth',0.01); %ploteo barras de error
+    errorbar(t_ss+(w_pre+w_post)*(ch-1),mean(spikeshapes_ch(ch).ch,2),desv_std,'color',[0 0 0 0],'LineWidth',0.01); %ploteo barras de error
     xlabel (desired_channels_neural(ch));
 end
     hold off
@@ -49,13 +49,11 @@ end
         estimulo=char(estimulo(1)); %para tenerlo una sola vez
         move_to_base_workspace(estimulo);
         
-        for i= 1:(ntrials(k)) %para todos los trials del estimulo
-        spikenumtrial(i)=numel(spike_lcs_ss{k}); %cuenta el número de spikes 
-        move_to_base_workspace(spikenumtrial);
-        numspikes= sum(spikenumtrial(1:i)); %y los suma para tener #spikes/trial
-        end
+        numspikes_stim(k)=numel(spike_lcs_ss{k}); %cuenta el número de spikes por estimulo 
+        move_to_base_workspace(numspikes_stim);
+      
 colnames={'Ave', 'Fecha', 'Protocolo', 'Estimulo','Profundidad', 'Canales', 'Umbral','Desvio', 'Spikes'};
-valuetable={ave, fecha, file, estimulo, profundidad, canales, thr,std_min, numspikes};       
+valuetable={ave, fecha, file, estimulo, profundidad, canales, thr,std_min, numspikes_stim(k)};       
 uitable(ss,'Data', valuetable, 'RowName', [], 'ColumnName', colnames,'Position', [50 30 1050 40.5]);
 
 end 
